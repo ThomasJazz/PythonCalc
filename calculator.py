7809edad2f554c154884de7cf4bf5f57c079063b
ThomasJazz <ttjazztt@gmail.com>
2018-10-23 18:29:35 -0700

    Started working on an implementation that uses postfix notation to solve equations

--- calculator.py
+++ calculator.py
@@ -1,145 +1,97 @@
 import math
-
-def inputParser(input):
-    # if there are no parenthesis in the expression then we can just evaluate the entire expression at face value
-    if "(" and ")" not in input:
-        calc_expression(input)
-
-    root = "sqrt"
-    # check for sqrt function
-    if root in input:
-        slice = find_str(root, input+len(root))
-        handleParen(input[slice:])
-
-    # initial check to make there are no illegal characters or expressions
-    leftParen = 0
-    rightParen = 0
-    for i in range(0, len(input)):
-        if not(input[i].isdigit() or isLegalChar(input[i])):
-            return False
-
-        if input[i] == "(":
-            leftParen += 1
-
-        if input[i] == ")":
-            if rightParen >= leftParen:
-                return "Error, illegal use of ')'"
-            rightParen += 1
-
-    # if we have more of one type of parenthesis than the other
-    if leftParen != rightParen:
-        return "Illegal usage of parenthesis"
-
-    for i in range(0, len(input)):
-
-        if input[i] == "(":
-            tempRev = reverse(input[i:])
-            j = 0
-            while not(tempRev[j]==")"):
-                j += 1
-
-            replaceMe = input[i:len(input)-j]
-            withMe = handleParen(input[i:len(input)-j])
-            input.replace(replaceMe,withMe)
+from pythonds.basic.stack import Stack
 
 
+def toPost(infix):
+    # dictionary containing our math operation priorities according to PEMDAS
+    priority = {
+        "^": 4,
+        "#": 4,  # this will be my representation of a square root
+        "*": 3,
+        "/": 3,
+        "%": 3,
+        "+": 2,
+        "-": 2,
+        "(": 1  # this isn't according to PEMDAS because they are used more as delimiters than operators
+    }
+    opStack = Stack()
+    postList = []
+    expression = infix.split()
 
-def handleParen(exp):
-    output = ""
+    # check if our value is a number or parenthesis
+    for val in expression:
+        if val in "0123456789":
+            postList.append(val)
+        elif val == '(':
+            opStack.push(val)
+        elif val == ')':
+            topToken = opStack.pop()
 
-    for i in range (1, len(exp)):
-        if exp[i] == ")":
-            return calc_expression(output)
-        elif exp[i] == "(":
-            output += handleParen(exp[i+1:])
+            while topToken != '(':
+                postList.append(topToken)
+                topToken = opStack.pop()
         else:
-            output += exp[i]
+            while (not opStack.isEmpty()) and (priority[opStack.peek()] >= priority[val]):
+                  postList.append(opStack.pop())
+            opStack.push(val)
+
+    while not opStack.isEmpty():
+        postList.append(opStack.pop())
+    return " ".join(postList)
 
 
-def isLegalChar(char):
-    legalChars = "()^*/+-"
+def calcExpression(rawInput):
+    expression = toPost(rawInput)
+
+    i = count = 0
+    left = right = operand = None
+    replaceStr = ""
+    newStr = ""
+    while i < len(expression) and len(expression) > 1:
+        replaceStr += expression[i]
+
+        if expression[i] == " ":
+            count += 1
+
+        if count == 0:
+            left += str(expression[i])
+        elif count == 2:
+            right = str(expression[i])
+
+        if isLegalOp(expression[i]):
+            operand = expression[i]
+            newStr = str(calculate(operand, left, right))
+            expression = expression.replace(replaceStr, newStr)
+            i = 0
+            count = 0
+
+        i += 1
+
+    return expression
+
+
+def calculate(operand, left, right):
+    left = float(left)
+    right = float(right)
+    try:
+        return {
+            '^': math.pow(left, right),
+            '*': left * right,
+            '/': left/right,
+            '+': left+right,
+            '-': left-right
+        }[operand]
+    except ValueError, e:
+        print "error", e
+
+
+def isLegalOp(char):
+    legalChars = "#%^*/+-"
 
     return char in legalChars
 
 
-# this will only be used for locating the sqrt function in an input string
-def find_str(char, str):
-    index = 0
-
-    if char in str:
-        c = char[0]
-        for ch in str:
-            if ch == c:
-                if str[index:index+len(char)] == char:
-                    return index
-
-            index += 1
-
-    return -1
-
-
-def reverse(input):
-    rev = ''
-    for i in range(len(input),0,-1):
-        rev += input[i - 1]
-
-    return rev
-
-
-def calc_expression(expression):
-    i = 0
-    while i < len(expression):
-        if i == len(expression):
-            return expression
-
-        if isLegalChar(expression[i]):
-            if (i == len(expression)-1):
-                return "Incorrect use of operand: " + expression[i]
-
-            lOperand = ""
-            for j in range(i-1,-1,-1):
-                if expression[j].isdigit():
-                    lOperand += expression[j]
-                elif expression[j] == '.':
-                    lOperand += expression[j]
-                else:
-                    break
-
-            lOperand = reverse(lOperand)
-
-            rOperand = ""
-            for j in range(i+1, len(expression)):
-                if expression[j].isdigit():
-                    rOperand += expression[j]
-                elif expression[j] == '.':
-                    rOperand += expression[j]
-                else:
-                    break
-
-            operation = expression[i]
-            replaceMe = "" + lOperand + operation + rOperand
-            withMe = str(calculate(operation,str(lOperand),str(rOperand)))
-            expression = expression.replace(replaceMe, withMe)
-            i = 0
-            if expression == withMe:
-                return withMe
-        else:
-            i += 1
-
-
-    return expression
-
-def calculate(operand, left, right):
-
-    return {
-        '^': math.pow(float(left),float(right)),
-        '*': float(left) * float(right),
-        '/': float(left)/float(right),
-        '+': float(left)+float(right),
-        '-': float(left)-float(right)
-    }[operand]
-
-
-
-#print(calcExpression("5/5+2-1"))
-print(inputParser("5*(4+3)"))
+print(calcExpression("5 / 5 + 2 - 1"))
+print(calcExpression("5 * 5"))
+print(calcExpression("( 5 + 5 ) ^ 2"))
+print(calcExpression("5 * 9 + 3 / ( 8 + 4 )"))
